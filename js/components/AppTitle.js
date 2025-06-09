fc1695cd067dcf4d6dbfac3e157340be872aab64
TheJollyLaMa <j@caffeinelamanna.com>
2025-06-09 09:17:58 -0400

    adding style to the ankhs

--- js/components/AppTitle.js
+++ js/components/AppTitle.js
@@ -13,9 +13,23 @@ export class AppTitle extends HTMLElement {
         <link rel="stylesheet" href="../css/header.css" />
         <div id="header-center">
             <h1 id="app-title">
-              <span class="title-symbol">::-⊡- ☥</span>
+              <span class="title-symbol">::-⊡-</span>
+              <span class="ankh-wrapper">
+                <span class="ankh-coin">☥</span>
+                <span class="sparkle sparkle-top">✨</span>
+                <span class="sparkle sparkle-bottom">✨</span>
+                <span class="sparkle sparkle-left">✨</span>
+                <span class="sparkle sparkle-right">✨</span>
+              </span>
               <span class="title-main"> Decent</span> 🦚 <span class="title-main"> Head </span>
-              <span class="title-symbol">☥ -⊡-::</span>
+              <span class="ankh-wrapper">
+                <span class="ankh-coin">☥</span>
+                <span class="sparkle sparkle-top">✨</span>
+                <span class="sparkle sparkle-bottom">✨</span>
+                <span class="sparkle sparkle-left">✨</span>
+                <span class="sparkle sparkle-right">✨</span>
+              </span>
+              <span class="title-symbol">-⊡-::</span>
             </h1>
             <h3 id="app-subtitle">(Another Decent Frankenstein)</h3>
         </div>
